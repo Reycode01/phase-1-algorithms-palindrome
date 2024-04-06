fab419e83712efb43c259a809143738a7a79368d
humphrey <writernic89@gmail.com>
2024-04-07 01:35:17 +0300

    add palindrome file

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+  // Convert the word to lowercase and remove non-alphanumeric characters
+  const cleanWord = word.toLowerCase().replace(/[^a-z0-9]/g, '');
+  
+  // Check if the cleaned word is equal to its reverse
+  return cleanWord === cleanWord.split('').reverse().join('');
 }
 
 module.exports = isPalindrome;
+
